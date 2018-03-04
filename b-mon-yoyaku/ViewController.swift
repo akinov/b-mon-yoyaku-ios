911ccb0353f068de3db70f7a1effda0b3cc507a3
akinov <ayumoto10@gmail.com>
2018-03-04 16:43:07 +0900

    Initial Commit

---
+++ b-mon-yoyaku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  b-mon-yoyaku
+//
+//  Created by akinov on 2018/03/04.
+//  Copyright © 2018年 akinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
