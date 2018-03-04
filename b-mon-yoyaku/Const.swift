65a5c885df9ceee687725ee7f5faa1aef654b925
akinov <ayumoto10@gmail.com>
2018-03-04 17:32:48 +0900

    webViewで表示できるようにした

---
+++ b-mon-yoyaku/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  b-mon-yoyaku
+//
+//  Created by akinov on 2018/03/04.
+//  Copyright © 2018年 akinov. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let url = "https://www.b-monster.jp/"
+}
--- b-mon-yoyaku/ViewController.swift
+++ b-mon-yoyaku/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    // MARK: Propaties
+    @IBOutlet weak var webView: WKWebView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+         webView.navigationDelegate = self
+        
+        // 表示するWEBサイトのURLを設定
+        let urlRequest = URLRequest(url: URL(string: Const.url)!)
+        // webViewで表示するWEBサイトの読み込みを開始
+        webView.load(urlRequest)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // WKNavigationDelegate Methods
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webView.evaluateJavaScript(
+            "jQuery('#home-btn').attr('href');",
+            completionHandler: { (html, error) -> Void in
+                print(html as? String)
+        })
+    }
 }
 
